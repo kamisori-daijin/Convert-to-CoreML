a5734fe95388b10c1c95569749d321c455fe1f2e
kamisori-daijin <208445078+kamisori-daijin@users.noreply.github.com>
2025-08-11 20:21:01 +0900

    Update __init__.py

--- convert_to_coreml/src/convert_to_coreml/__init__.py
+++ convert_to_coreml/src/convert_to_coreml/__init__.py
@@ -1,12 +1,11 @@
 # src/convert_to_coreml/__init__.py
-
-
 from importlib.metadata import version, PackageNotFoundError
+
 from .convert import main
 
-__all__ = ["main", "__version__"]
+__all__ = ["main"]
 
 try:
-    __version__ = version(__name__)
+    __version__ = version("convert_to_coreml")
 except PackageNotFoundError:
     __version__ = "1.0.0"
